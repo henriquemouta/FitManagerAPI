31ef4db29ff9ac9cd8cbf3cc96cf5a1f13797dab
Erick Macedo <erickmacedo@edu.unifor.br>
2026-05-30 16:58:23 -0300

    Fix Program.cs

--- FitManagerAPI/Program.cs
+++ FitManagerAPI/Program.cs
@@ -36,7 +36,6 @@ if (app.Environment.IsDevelopment())
     });
 }
 
-// Ap�s builder.Build()
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
